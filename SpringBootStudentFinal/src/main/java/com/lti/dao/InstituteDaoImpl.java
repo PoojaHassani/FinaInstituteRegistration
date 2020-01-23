098e83558fba7d29a9fd39e23aebdd9f21f7a983
PoojaHassani <59229794+PoojaHassani@users.noreply.github.com>
2020-01-24 01:42:11 +0530

    Add files via upload

---
+++ SpringBootStudentFinal/src/main/java/com/lti/dao/InstituteDaoImpl.java
@@ -0,0 +1,39 @@
+package com.lti.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.lti.model.Institute;
+@Repository
+public class InstituteDaoImpl implements InstituteDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public int createInstitute(Institute institute) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(institute);
+		entityManager.getTransaction().commit();
+		return 1;
+	}
+
+	@Override
+	public void beginTransaction() {
+		entityManager.getTransaction().begin();
+
+	}
+
+	@Override
+	public void commitTransaction() {
+		entityManager.getTransaction().commit();
+
+	}
+
+	@Override
+	public void rollBackTransaction() {
+		entityManager.getTransaction().rollback();
+	}
+}
